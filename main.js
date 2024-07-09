4af2b252cb6fb695577a6cbdea470e42c7ed9a12
kurotamashi30 <kurotamashi30@gmail.com>
2024-07-09 11:14:53 +0900

    test

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 let news=[]
 const getLatestNews = async () => {
-    const url = `https://main--kaleidoscopic-beignet-ca4459.netlify.app/top-headlines `
+    const url = `https://main--kaleidoscopic-beignet-ca4459.netlify.app/top-headlines`
     try {
         const response = await fetch(url);
         const data = await response.json();
