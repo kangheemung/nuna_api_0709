eb387e83e2a6904e2b350b47e2870517682d72e5
kurotamashi30 <kurotamashi30@gmail.com>
2024-07-09 11:18:00 +0900

    done

--- main.js
+++ main.js
@@ -7,8 +7,35 @@ const getLatestNews = async () => {
         const data = await response.json();
         console.log(data);
         newsList = data.articles;
+        render();
     } catch (error) {
         console.error('Error fetching or parsing data:', error.message);
     }
 };
 getLatestNews();
+const render = () => {
+    const newsHTML = newsList.map(news => {
+        const imageSrc = news.urlToImage || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU";
+        const summary = news.description && news.description.length > 200 ? news.description.substring(0, 200) + '...' : news.description || '내용없음';
+        const sourceName = news.source.name || 'no source';
+        const publishedTime = moment(news.publishedAt, "YYYYMMDD").fromNow();
+
+        return `<div class ="row news">
+                    <div class ="col-lg-4">
+                        <img class="news_size"
+                        src=${imageSrc} "/>
+                    </div>
+                    <div class ="col-lg-8">
+                        <h2>${news.title}</h2>
+                        <p>
+                         ${summary}
+                        </p>
+                        <div>
+                            ${sourceName} ${publishedTime}
+                        </div>
+                    </div>
+                </div>`;
+        }).join("");
+
+    document.getElementById("news-board").innerHTML = newsHTML;
+  };
