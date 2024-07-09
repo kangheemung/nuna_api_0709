3b8b31b1d11f4cce5048cb6554240878e3a03a0c
kurotamashi30 <kurotamashi30@gmail.com>
2024-07-09 12:56:28 +0900

    button

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-
+//버튼들에 클릭이벤트를 주기
+//카테고리별 뉴스 가져오기
+//그 뉴스를 보여주기렌더
+const menus= document.querySelectorAll(".menus button")
+menus.forEach(menus=>menus.addEventListener("click",(e=>getNewsByCategory(e))))
+console.log(menus);
 let news=[]
 const getLatestNews = async () => {
     const url = `https://main--kaleidoscopic-beignet-ca4459.netlify.app/top-headlines`
@@ -13,6 +18,43 @@ const getLatestNews = async () => {
     }
 };
 getLatestNews();
+const getNewsByCategory = (e) => {
+    const category = e.target.textContent.toLowerCase();
+      console.log("category", category);
+    const url = new URL(
+        `https://main--kaleidoscopic-beignet-ca4459.netlify.app/top-headlines?category=${category}`
+    );
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log("Data", data);
+            newsList = data.articles;
+            render()
+        })
+        .catch(error => console.error('Error fetching or parsing data:', error.message));
+};
+
+const getNewsByKeyword = (keyword) => {
+    if (typeof keyword === 'undefined') {
+        const keyword = document.getElementById("search-input").value;
+    }
+
+      console.log("keyword", keyword);
+
+     const url = new URL(` https://newsapi.org/v2/top-headlines?q=${keyword}`);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log("Data for keyword", keyword, ":", data);
+            newsList = data.articles;
+            console.log(newsList);
+            render();
+        })
+        .catch(error => console.error('Error fetching or parsing data:', error.message));
+};
+
+
 const render = () => {
     const newsHTML = newsList.map(news => {
         const imageSrc = news.urlToImage || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU";
